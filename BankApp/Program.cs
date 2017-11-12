a0fb417f0f35d057eb648262913298fc08f40e1d
anitha laksharam <anithalaksharam@gmail.com>
2017-11-11 18:52:36 -0800

    Initial Commit of Bank App

---
+++ BankApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //referenceobject, instance of a class
+            var account = new Account();
+            account.EmailAddress = "test@test.com";
+            account.AccountType = "Checking";
+
+            account.Deposit(100.10M);
+            Console.WriteLine($"{account.Balance}");
+   
+
+        }
+    }
+}
